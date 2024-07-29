3452aecea47274412a89da127dd0ca494e374a30
Anthony Re <anthonyjosephre@gmail.com>
2024-07-29 22:28:19 +0000

    introducing avro encoding and decoding to the route handlers

---
+++ internal/commitlog/error.go
@@ -0,0 +1,14 @@
+package commitlog
+
+import (
+	"fmt"
+)
+
+// RecordNotFound occurs when a given offset does not contain a record.
+type RecordNotFound struct {
+	Offset uint64
+}
+
+func (r RecordNotFound) Error() string {
+	return fmt.Sprintf("record not found at offset: %d", r.Offset)
+}
---
+++ internal/schema/schema.go
@@ -0,0 +1,25 @@
+// package schema provides mechanisms to encode and decode avros schemas. Avro
+// schemas are located under directories corresponding to the package defining
+// the data type (e.g internal/commitlog <=> schemas/commitlog)
+package schema
+
+import (
+	"os"
+
+	"github.com/linkedin/goavro"
+)
+
+// Provide paths to schemas for convenience and future-proofing.
+const (
+	COMMIT_LOG_RECORD = "commitlog/record.json"
+)
+
+// MakeCodec accepts a path to an avro schema and produces a [goavro.Codec].
+func MakeCodec(path string) (*goavro.Codec, error) {
+	data, err := os.ReadFile(path)
+	if err != nil {
+		return nil, err
+	}
+
+	return goavro.NewCodec(string(data))
+}
